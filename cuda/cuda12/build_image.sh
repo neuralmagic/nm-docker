60ae6530c733992034714322808b249aee6f230b
Dipika Sikka <dipikasikka1@gmail.coom>
2024-03-19 15:49:44 +0000

    update/add dockerfiles for cuda 12.1.0

--- cuda/cuda12/build_image.sh
+++ cuda/cuda12/build_image.sh
@@ -1,3 +1,3 @@
 DOCKER_BUILDKIT=1 \
-  docker build --build-arg CUDA_VERSION="12.1.0" -t ghcr.io/neuralmagic/base-cuda12.1.0 ../cuda11/
+  docker build --build-arg CUDA_VERSION="12.1.0" -t ghcr.io/neuralmagic/base-cuda12.1.0 .
 
